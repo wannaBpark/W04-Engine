7c030677bd7f325afbdad8b2b105bba39cebbe6a
gudtldn <gudtldn1234@gmail.com>
2025-03-20 21:13:33 +0900

    feat: FName 및 FNamePool 클래스 추가 및 기존 NameTypes.h 파일 삭제

---
+++ W03StaticMesh_1/Week0v2/Engine/Source/Runtime/CoreUObject/UObject/NameTypes.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+#include "Core/HAL/PlatformType.h"
+
+class FString;
+
+
+class FName
+{
+    friend struct FNameHelper;
+
+    uint32 DisplayIndex;    // 원본 문자열의 Hash
+    uint32 ComparisonIndex; // 비교시 사용되는 Hash
+
+public:
+    FName() : DisplayIndex(0), ComparisonIndex(0) {}
+    FName(const WIDECHAR* Name);
+    FName(const ANSICHAR* Name);
+    FName(const FString& Name);
+
+    FString ToString() const;
+    uint32 GetDisplayIndex() const { return DisplayIndex; }
+    uint32 GetComparisonIndex() const { return ComparisonIndex; }
+
+    bool operator==(const FName& Other) const;
+};
