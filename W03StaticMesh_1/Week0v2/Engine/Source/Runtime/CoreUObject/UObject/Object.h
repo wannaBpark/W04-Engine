0bd172062b3b984200b673c4fd12933490bf09a9
gudtldn <gudtldn1234@gmail.com>
2025-03-20 21:24:23 +0900

    feat: UObject 클래스의 메서드 정리 및 FName 반환 메서드 추가

--- W03StaticMesh_1/Week0v2/Engine/Source/Runtime/CoreUObject/UObject/Object.h
+++ W03StaticMesh_1/Week0v2/Engine/Source/Runtime/CoreUObject/UObject/Object.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Define.h"
 #include "EngineLoop.h"
+#include "NameTypes.h"
 #include "Engine/Source/Runtime/Engine/World.h"
 #include "UClass.h"
 
@@ -8,30 +9,55 @@ extern FEngineLoop GEngineLoop;
 
 class UClass;
 class UWorld;
+
+
 class UObject
 {
 public:
     UObject();
     UObject(FString& name);
     UObject(FString& name, uint32 uuid);
-    virtual				~UObject();
+    virtual ~UObject();
 
-    virtual void		Initialize();
-    virtual void		Update(double deltaTime);
-    virtual void		Release();
-    virtual void		Render();
-    virtual void		RenderUUID();
+    virtual void Initialize();
+    virtual void Update(double deltaTime);
+    virtual void Release();
+    virtual void Render();
+    virtual void RenderUUID();
 
-    inline UWorld*				GetWorld() { return GEngineLoop.GetWorld(); }
-    inline FEngineLoop&			GetEngine() { return GEngineLoop; }
-    inline void					SetName(FString& _Name) { Name = FName(_Name); }
-    inline FName&				GetName() { return Name; }
+    inline UWorld* GetWorld()
+    {
+        return GEngineLoop.GetWorld();
+    }
+
+    inline FEngineLoop& GetEngine()
+    {
+        return GEngineLoop;
+    }
+
+    inline void SetName(const FString& _Name)
+    {
+        Name = FName(_Name);
+    }
+
+    inline FName GetFName() const
+    {
+        return Name;
+    }
+
+    inline FString GetName() const
+    {
+        return Name.ToString();
+    }
+
 public:
     uint32 UUID;
     uint32 InternalIndex; // Index of GUObjectArray
     FName Name;
+
 public:
-    static void* operator new(size_t size) {
+    static void* operator new(size_t size)
+    {
         UE_LOG(LogLevel::Display, "UObject Created : %d", size);
         FEngineLoop::TotalAllocationBytes += static_cast<uint32>(size);
         FEngineLoop::TotalAllocationCount++;
@@ -39,16 +65,21 @@ public:
         UE_LOG(LogLevel::Display, "TotalAllocationBytes : %d, TotalAllocationCount : %d", FEngineLoop::TotalAllocationBytes, FEngineLoop::TotalAllocationCount);
         return std::malloc(size);
     }
-    static void operator delete(void* ptr) {
+
+    static void operator delete(void* ptr)
+    {
         std::free(ptr);
     }
+
     virtual UClass* GetClass() const = 0;
 
-    bool IsA(UClass* TargetClass) const; 
-    static UClass* StaticClass() {
+    bool IsA(UClass* TargetClass) const;
+
+    static UClass* StaticClass()
+    {
         static UClass ClassInfo("UObject", nullptr);
         return &ClassInfo;
     }
-private:
-};	
 
+private:
+};
