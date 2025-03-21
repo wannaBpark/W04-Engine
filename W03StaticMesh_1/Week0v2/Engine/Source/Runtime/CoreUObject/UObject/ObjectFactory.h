20002ef68037a12bc408f021eba515920d3da08d
gudtldn <gudtldn1234@gmail.com>
2025-03-21 13:59:58 +0900

    feat: ConstructObject 템플릿 함수 정리 및 중복 코드 제거

--- W03StaticMesh_1/Week0v2/Engine/Source/Runtime/CoreUObject/UObject/ObjectFactory.h
+++ W03StaticMesh_1/Week0v2/Engine/Source/Runtime/CoreUObject/UObject/ObjectFactory.h
@@ -19,20 +19,6 @@ public:
     }
 
     template<class T>
-    static UObject* ConstructObject(FString& Name)
-    {
-        uint32 id = UEngineStatics::GenUUID();
-        Name = Name + "_" + std::to_string(id);
-        UE_LOG(LogLevel::Display, "Created New Object : %s", Name);
-        UObject* obj = new T;
-        obj->SetName(Name);
-        obj->UUID = id;
-        obj->Initialize();
-
-        return obj;
-    }
-
-    template<class T>
     static UObject* ConstructObject(const FString& Name)
     {
         uint32 id = UEngineStatics::GenUUID();
@@ -40,19 +26,6 @@ public:
         UE_LOG(LogLevel::Display, "Created New Object : %s", newName);
         UObject* obj = new T;
         obj->SetName(Name);
-        obj->UUID = id;
-        obj->Initialize();
-
-        return obj;
-    }
-    template<class T>
-    static UObject* ConstructObject(const char* Name)
-    {
-        uint32 id = UEngineStatics::GenUUID();
-        FString newName = FString(Name) + "_" + std::to_string(id);
-        UE_LOG(LogLevel::Display, "Created New Object : %s", *newName);
-        UObject* obj = new T;
-        obj->SetName(newName);
         obj->UUID = id;
         obj->Initialize();
 
