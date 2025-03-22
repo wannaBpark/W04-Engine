0f6d2134a02b3c3dc72151795976984f4806fea4
gudtldn <gudtldn1234@gmail.com>
2025-03-22 11:27:31 +0900

    refactor: 수학 관련 헤더 파일의 주석을 한국어로 수정 및 정리

--- W03StaticMesh_1/Week0v2/Engine/Source/Runtime/Core/Math/Vector.h
+++ W03StaticMesh_1/Week0v2/Engine/Source/Runtime/Core/Math/Vector.h
@@ -2,58 +2,58 @@
 
 #include <DirectXMath.h>
 
-// 3D ∫§≈Õ
+// 3D Î≤°ÌÑ∞
 struct FVector
 {
-	float x, y, z;
-	FVector(float _x = 0, float _y = 0, float _z = 0) : x(_x), y(_y), z(_z) {}
+    float x, y, z;
+    FVector(float _x = 0, float _y = 0, float _z = 0) : x(_x), y(_y), z(_z) {}
 
-	FVector operator-(const FVector& other) const {
-		return FVector(x - other.x, y - other.y, z - other.z);
-	}
-	FVector operator+(const FVector& other) const {
-		return FVector(x + other.x, y + other.y, z + other.z);
-	}
+    FVector operator-(const FVector& other) const {
+        return FVector(x - other.x, y - other.y, z - other.z);
+    }
+    FVector operator+(const FVector& other) const {
+        return FVector(x + other.x, y + other.y, z + other.z);
+    }
 
-	// ∫§≈Õ ≥ª¿˚
-	float Dot(const FVector& other) const {
-		return x * other.x + y * other.y + z * other.z;
-	}
+    // Î≤°ÌÑ∞ ÎÇ¥Ï†Å
+    float Dot(const FVector& other) const {
+        return x * other.x + y * other.y + z * other.z;
+    }
 
-	// ∫§≈Õ ≈©±‚
-	float Magnitude() const {
-		return sqrt(x * x + y * y + z * z);
-	}
+    // Î≤°ÌÑ∞ ÌÅ¨Í∏∞
+    float Magnitude() const {
+        return sqrt(x * x + y * y + z * z);
+    }
 
-	// ∫§≈Õ ¡§±‘»≠
-	FVector Normalize() const {
-		float mag = Magnitude();
-		return (mag > 0) ? FVector(x / mag, y / mag, z / mag) : FVector(0, 0, 0);
-	}
-	FVector Cross(const FVector& Other) const
-	{
-		return FVector{
-			y * Other.z - z * Other.y,
-			z * Other.x - x * Other.z,
-			x * Other.y - y * Other.x
-		};
-	}
-	// Ω∫ƒÆ∂Û ∞ˆº¿
-	FVector operator*(float scalar) const {
-		return FVector(x * scalar, y * scalar, z * scalar);
-	}
+    // Î≤°ÌÑ∞ Ï†ïÍ∑úÌôî
+    FVector Normalize() const {
+        float mag = Magnitude();
+        return (mag > 0) ? FVector(x / mag, y / mag, z / mag) : FVector(0, 0, 0);
+    }
+    FVector Cross(const FVector& Other) const
+    {
+        return FVector{
+            y * Other.z - z * Other.y,
+            z * Other.x - x * Other.z,
+            x * Other.y - y * Other.x
+        };
+    }
+    // Ïä§ÏπºÎùº Í≥±ÏÖà
+    FVector operator*(float scalar) const {
+        return FVector(x * scalar, y * scalar, z * scalar);
+    }
 
-	bool operator==(const FVector& other) const {
-		return (x == other.x && y == other.y && z == other.z);
-	}
+    bool operator==(const FVector& other) const {
+        return (x == other.x && y == other.y && z == other.z);
+    }
 
-	float Distance(const FVector& other) const {
-		// µŒ ∫§≈Õ¿« ¬˜ ∫§≈Õ¿« ≈©±‚∏¶ ∞ËªÍ
-		return ((*this - other).Magnitude());
-	}
-	DirectX::XMFLOAT3 ToXMFLOAT3() const
-	{
-		return DirectX::XMFLOAT3(x, y, z);
-	}
+    float Distance(const FVector& other) const {
+        // Îëê Î≤°ÌÑ∞Ïùò Ï∞® Î≤°ÌÑ∞Ïùò ÌÅ¨Í∏∞Î•º Í≥ÑÏÇ∞
+        return ((*this - other).Magnitude());
+    }
+    DirectX::XMFLOAT3 ToXMFLOAT3() const
+    {
+        return DirectX::XMFLOAT3(x, y, z);
+    }
 
 };
--- W03StaticMesh_1/Week0v2/Engine/Source/Runtime/Core/Math/Vector4.h
+++ W03StaticMesh_1/Week0v2/Engine/Source/Runtime/Core/Math/Vector4.h
@@ -1,18 +1,18 @@
 #pragma once
 
-// 4D ∫§≈Õ
+// 4D Î≤°ÌÑ∞
 struct FVector4 {
-	float x, y, z, a;
-	FVector4(float _x = 0, float _y = 0, float _z = 0, float _a = 0) : x(_x), y(_y), z(_z), a(_a) {}
+    float x, y, z, a;
+    FVector4(float _x = 0, float _y = 0, float _z = 0, float _a = 0) : x(_x), y(_y), z(_z), a(_a) {}
 
-	FVector4 operator-(const FVector4& other) const {
-		return FVector4(x - other.x, y - other.y, z - other.z, a - other.a);
-	}
-	FVector4 operator+(const FVector4& other) const {
-		return FVector4(x + other.x, y + other.y, z + other.z, a + other.a);
-	}
-	FVector4 operator/(float scalar) const
-	{
-		return FVector4{ x / scalar, y / scalar, z / scalar, a / scalar };
-	}
+    FVector4 operator-(const FVector4& other) const {
+        return FVector4(x - other.x, y - other.y, z - other.z, a - other.a);
+    }
+    FVector4 operator+(const FVector4& other) const {
+        return FVector4(x + other.x, y + other.y, z + other.z, a + other.a);
+    }
+    FVector4 operator/(float scalar) const
+    {
+        return FVector4{ x / scalar, y / scalar, z / scalar, a / scalar };
+    }
 };
