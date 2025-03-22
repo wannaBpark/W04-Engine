b28d5f4b1711d7771c39b6e2f773fc5c50db11f9
gudtldn <gudtldn1234@gmail.com>
2025-03-22 20:49:48 +0900

    feat: UObjectHash.cpp 및 UObjectHash.h 파일 추가 및 GetObjectsOfClass 함수 정의

---
+++ W03StaticMesh_1/Week0v2/Engine/Source/Runtime/CoreUObject/UObject/UObjectHash.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+#include "Object.h"
+#include "Container/Array.h"
+
+class UClass;
+
+/**
+ * ClassToLookFor와 일치하는 UObject를 반환합니다.
+ * @param ClassToLookFor 반환할 Object의 CLass정보
+ * @param Results Objects
+ */
+void GetObjectsOfClass(const UClass* ClassToLookFor, TArray<UObject*>& Results);
