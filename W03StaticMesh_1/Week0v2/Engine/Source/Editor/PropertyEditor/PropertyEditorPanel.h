f1bff1b7a5e16d8c2ad1a20461d8b6d9b1196e4e
Xerlocked <xhouse.xman@gmail.com>
2025-03-26 00:55:12 +0900

    update: Material Viewer

--- W03StaticMesh_1/Week0v2/Engine/Source/Editor/PropertyEditor/PropertyEditorPanel.h
+++ W03StaticMesh_1/Week0v2/Engine/Source/Editor/PropertyEditor/PropertyEditorPanel.h
@@ -15,10 +15,17 @@ public:
 private:
     void RGBToHSV(float r, float g, float b, float& h, float& s, float& v);
     void HSVToRGB(float h, float s, float v, float& r, float& g, float& b);
+
+    /* Materials Settings */
     void RenderForStaticMesh(UStaticMeshComponent* StaticMeshComp);
+    void RenderMaterialView(UMaterial* Material);
 private:
     float Width = 0, Height = 0;
     FVector Location = FVector(0, 0, 0);
     FVector Rotation = FVector(0, 0, 0);
     FVector Scale = FVector(0, 0, 0);
+
+    /* Material Property */
+    uint32 SelectedMaterialIndex = -1;
+    UStaticMeshComponent* SelectedStaticMeshComp = nullptr;
 };
