a9e1afa75b922879e9252cd800d540505c33d7a5
gudtldn <gudtldn1234@gmail.com>
2025-03-25 19:43:12 +0900

    feat: FVector에 ForwardVector, RightVector, UpVector 추가 및 EditorViewportClient에서 사용

--- W03StaticMesh_1/Week0v2/Engine/Source/Runtime/Core/Math/Vector.cpp
+++ W03StaticMesh_1/Week0v2/Engine/Source/Runtime/Core/Math/Vector.cpp
@@ -2,3 +2,6 @@
 
 const FVector FVector::ZeroVector = {0.0f, 0.0f, 0.0f};
 const FVector FVector::OneVector = {1.0f, 1.0f, 1.0f};
+const FVector FVector::ForwardVector = {1.0f, 0.0f, 0.0f};
+const FVector FVector::RightVector = {0.0f, 1.0f, 0.0f};
+const FVector FVector::UpVector = {0.0f, 0.0f, 1.0f};
