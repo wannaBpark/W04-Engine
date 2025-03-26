ffeef4f8e40f6ebd779851e972dd7a418894db94
gudtldn <gudtldn1234@gmail.com>
2025-03-26 22:16:56 +0900

    fix: ObjectMacros.h에서 코드 정렬 및 공백 수정

--- W03StaticMesh_1/Week0v2/Engine/Source/Runtime/CoreUObject/UObject/ObjectMacros.h
+++ W03StaticMesh_1/Week0v2/Engine/Source/Runtime/CoreUObject/UObject/ObjectMacros.h
@@ -9,17 +9,17 @@
 // RTTI를 위한 클래스 매크로
 #define DECLARE_CLASS(TClass, TSuperClass) \
 private: \
-TClass(const TClass&) = delete; \
-TClass& operator=(const TClass&) = delete; \
-TClass(TClass&&) = delete; \
-TClass& operator=(TClass&&) = delete; \
+    TClass(const TClass&) = delete; \
+    TClass& operator=(const TClass&) = delete; \
+    TClass(TClass&&) = delete; \
+    TClass& operator=(TClass&&) = delete; \
 public: \
-using Super = TSuperClass; \
-using ThisClass = TClass; \
-static UClass* StaticClass() { \
-static UClass ClassInfo{ TEXT(#TClass), static_cast<uint32>(sizeof(TClass)), static_cast<uint32>(alignof(TClass)), TSuperClass::StaticClass() }; \
-return &ClassInfo; \
-}
+    using Super = TSuperClass; \
+    using ThisClass = TClass; \
+    static UClass* StaticClass() { \
+        static UClass ClassInfo{ TEXT(#TClass), static_cast<uint32>(sizeof(TClass)), static_cast<uint32>(alignof(TClass)), TSuperClass::StaticClass() }; \
+        return &ClassInfo; \
+    }
 
 
 // #define PROPERTY(Type, VarName, DefaultValue) \
@@ -31,5 +31,5 @@ return &ClassInfo; \
 
 // Getter & Setter 생성
 #define PROPERTY(type, name) \
-void Set##name(const type& value) { name = value; } \
-type Get##name() const { return name; }
+    void Set##name(const type& value) { name = value; } \
+    type Get##name() const { return name; }
