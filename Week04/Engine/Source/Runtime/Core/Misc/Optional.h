3bcfbf45fea4b1537c3c70b8274359321de2312e
gudtldn <gudtldn1234@gmail.com>
2025-03-29 20:53:55 +0900

    wip

---
+++ Week04/Engine/Source/Runtime/Core/Misc/Optional.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+
+template <typename OptionalType>
+struct TOptional
+{
+public:
+    using ElementType = OptionalType;
+
+    TOptional(const OptionalType& Other)
+        
+    {
+    }
+
+private:
+    alignas(OptionalType) OptionalType Value;
+};
